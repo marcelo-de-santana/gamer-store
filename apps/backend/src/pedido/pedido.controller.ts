51092ee5b7535c2f74c2858e5425afbb52c9bd0b
marcelo-de-santana <marcelondesantana@outlook.com>
2024-11-28 08:34:30 -0300

    Finalizando módulo de pedido no backend

--- apps/backend/src/pedido/pedido.controller.ts
+++ apps/backend/src/pedido/pedido.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { PedidoPrisma } from './pedido.prisma';
+import { Pedido } from '@gstore/core';
 
-@Controller('pedido')
-export class PedidoController {}
+@Controller('pedidos')
+export class PedidoController {
+  constructor(private readonly repository: PedidoPrisma) {}
+
+  @Post()
+  async salvar(@Body() pedido: Pedido) {
+    return this.repository.salvar(pedido);
+  }
+
+  @Get()
+  async obterPedidos() {
+    return this.repository.obter();
+  }
+
+  @Get(':id')
+  async obterPedidoPorId(@Param('id') id: string) {
+    return this.repository.obterPorId(+id);
+  }
+
+  @Delete(':id')
+  async excluirPedido(@Param('id') id: string) {
+    return this.repository.excluir(+id);
+  }
+}
---
+++ apps/backend/src/pedido/pedido.prisma.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaProvider } from '../db/prisma.provider';
+import { Pedido } from '@gstore/core';
+
+@Injectable()
+export class PedidoPrisma {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  async obter() {
+    const pedidos = await this.prisma.pedido.findMany();
+    return pedidos;
+  }
+
+  async obterPorId(id: number) {
+    const pedidos = await this.prisma.pedido.findUnique({
+      where: { id },
+      include: {
+        itens: { include: { produto: { select: { id: true, nome: true } } } },
+        entrega: true,
+      },
+    });
+    return pedidos;
+  }
+
+  async salvar(pedido: Pedido) {
+    await this.prisma.pedido.create({
+      data: {
+        data: pedido.data,
+        status: pedido.status,
+        valorTotal: pedido.valorTotal,
+        formaPagamento: pedido.formaPagamento,
+        entrega: { create: { ...pedido.entrega } },
+        itens: {
+          create: pedido.itens.map((item) => ({
+            produtoId: item.produto.id,
+            precoUnitario: item.precoUnitario,
+            quantidade: item.quantidade,
+          })),
+        },
+      },
+    });
+  }
+
+  async excluir(id: number) {
+    const pedido = await this.prisma.pedido.findUnique({
+      where: { id },
+    });
+
+    if (!pedido) return;
+    await this.prisma.$transaction([
+      this.prisma.itemPedido.deleteMany({ where: { pedidoId: id } }),
+      this.prisma.pedido.delete({ where: { id } }),
+      this.prisma.pedidoEntrega.delete({ where: { id: pedido.entregaId } }),
+    ]);
+  }
+}
